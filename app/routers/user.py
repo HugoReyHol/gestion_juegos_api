7b03be800cf66be091311adc17a58b7f2074738b
Hugo <hugo.reyhol@educa.jcyl.es>
2025-02-08 23:46:03 +0100

    registro encripta e inicio devuelve token

--- app/routers/user.py
+++ app/routers/user.py
@@ -1,23 +1,31 @@
+from datetime import timedelta
+from typing import Annotated
 from fastapi import APIRouter, HTTPException, Depends
+from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
 from app.db.database import get_db
 from app.schemas.user import UserCreate, UserResponse
 from app.db.models import User
+from app.security.jwt_util import ACCESS_TOKEN_EXPIRE_MINUTES, create_access_token, hash_password, verify_password
 
 router = APIRouter(
     prefix="/user",
     tags=["Users"]
 )
 
-@router.get("/{username}", response_model=UserResponse)
-def get_user(username: str, db:Session=Depends(get_db)) -> UserResponse:
-    user = db.query(User).filter(User.username == username).first()
+@router.post("/login")
+def get_user(form_data: Annotated[OAuth2PasswordRequestForm, Depends()], db:Session=Depends(get_db)):
+    user = db.query(User).filter(User.username == form_data.username).first()
     if not user:
         raise HTTPException(status_code=404, detail="User not found")
+    if not verify_password(plain_password=form_data.password, hashed_password=user.password):
+        raise HTTPException(status_code=401, detail="Wrong credentials")
 
-    return user
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(data={"sub": user.username}, expires_delta=access_token_expires)
+    return {"access_token": access_token, "token_type": "bearer"}
 
-@router.post("/")
+@router.post("/insert")
 def insert_user(user: UserCreate, db:Session=Depends(get_db)) -> int:
     existing_user = db.query(User).filter(User.username == user.username).first()
     if existing_user:
@@ -25,7 +33,7 @@ def insert_user(user: UserCreate, db:Session=Depends(get_db)) -> int:
 
     new_user = User(
         username = user.username,
-        password = user.password
+        password = hash_password(user.password)
     )
     db.add(new_user)
     db.commit()
---
+++ app/security/jwt_util.py
@@ -0,0 +1,40 @@
+from datetime import datetime, timedelta
+from typing import Optional
+from jose import JWTError, jwt
+from fastapi import Depends, HTTPException
+from fastapi.security import OAuth2PasswordBearer
+from passlib.context import CryptContext
+
+# Constantes
+SECRET_KEY = "clave_secreta"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="auth/login")
+
+# Función para encriptar contraseñas
+def hash_password(password: str) -> str:
+    return pwd_context.hash(password)
+
+# Función para verificar contraseñas
+def verify_password(plain_password: str, hashed_password: str) -> bool:
+    return pwd_context.verify(plain_password, hashed_password)
+
+# Función para generar un JWT
+def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
+    to_encode = data.copy()
+    expire = datetime.utcnow() + (expires_delta or timedelta(minutes=15))
+    to_encode.update({"exp": expire})
+    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+
+# Función para validar el token
+def get_current_user(token: str = Depends(oauth2_scheme)):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None:
+            raise HTTPException(status_code=401, detail="Token inválido")
+        return {"username": username}
+    except JWTError:
+        raise HTTPException(status_code=401, detail="Token inválido o expirado")
