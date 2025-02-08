ce1de3388c13a153c14d485eea5f7560f2773578
Hugo <hugo.reyhol@educa.jcyl.es>
2025-02-08 21:07:47 +0100

    Precarga juegos al iniciar la db por primera vez

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 import uvicorn
+from app.db.db_initialize import initialize_database
 from app.routers import user, game, user_game
 from app.db.database import Base, engine
 
@@ -7,6 +8,7 @@ def create_tables():
     Base.metadata.create_all(bind=engine)
 
 create_tables()
+initialize_database()
 
 app: FastAPI = FastAPI()
 app.include_router(user.router)
