27ed0ccf2c3736fda50aba713ef48eefd9dd7f4d
Hugo <hugo.reyhol@educa.jcyl.es>
2025-02-09 11:55:58 +0100

    get_current_user devuelve el user

--- app/routers/user.py
+++ app/routers/user.py
@@ -3,7 +3,7 @@ from fastapi import APIRouter, HTTPException, Depends
 from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
 from app.db.database import get_db
-from app.schemas.user import UserCreate
+from app.schemas.user import UserCreate, UserResponse
 from app.db.models import User
 from app.security.jwt_util import get_current_user, ACCESS_TOKEN_EXPIRE_MINUTES, create_access_token
 
@@ -12,12 +12,20 @@ router = APIRouter(
     tags=["Users"]
 )
 
-@router.get("/me")
+# Función para probar el token
+@router.get("/me", response_model=UserResponse)
 async def read_users_me(current_user: User = Depends(get_current_user)):
-    return current_user
+    print(current_user.idUser)
+    print(current_user.username)
+    print(current_user.password)
+    return UserResponse(
+        idUser=current_user.idUser,
+        username=current_user.username,
+        password=current_user.password,
+    )
 
 @router.post("/login")
-async def get_user(form_data: OAuth2PasswordRequestForm = Depends(), db:Session=Depends(get_db)):
+def get_user(form_data: OAuth2PasswordRequestForm = Depends(), db:Session=Depends(get_db)):
     user = db.query(User).filter(User.username == form_data.username).first()
     if not user:
         raise HTTPException(status_code=404, detail="User not found")
@@ -25,7 +33,7 @@ async def get_user(form_data: OAuth2PasswordRequestForm = Depends(), db:Session=
         raise HTTPException(status_code=401, detail="Wrong credentials")
 
     access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
-    access_token = await create_access_token(data={"sub": user.username}, expires_delta=access_token_expires)
+    access_token = create_access_token(data={"sub": user.username}, expires_delta=access_token_expires)
     return {"access_token": access_token, "token_type": "bearer"}
 
 @router.post("/insert")
--- app/security/jwt_util.py
+++ app/security/jwt_util.py
@@ -1,8 +1,12 @@
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, timezone
 from typing import Optional
 from jose import JWTError, jwt
 from fastapi import Depends, HTTPException
 from fastapi.security import OAuth2PasswordBearer
+from sqlalchemy.orm import Session
+
+from app.db.database import get_db
+from app.db.models import User
 
 # Constantes
 SECRET_KEY = "clave_secreta"
@@ -12,19 +16,23 @@ ACCESS_TOKEN_EXPIRE_MINUTES = 30
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="auth/login")
 
 # Función para generar un JWT
-async def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
+def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
     to_encode = data.copy()
-    expire = datetime.utcnow() + (expires_delta or timedelta(minutes=15))
+    expire = datetime.now(timezone.utc) + (expires_delta or timedelta(minutes=15))
     to_encode.update({"exp": expire})
     return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
 
 # Función para validar el token
-async def get_current_user(token: str = Depends(oauth2_scheme)):
+async def get_current_user(token: str = Depends(oauth2_scheme), db:Session=Depends(get_db)):
     try:
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
         username: str = payload.get("sub")
         if username is None:
             raise HTTPException(status_code=401, detail="Token inválido")
-        return {"username": username}
+        user = db.query(User).filter(User.username == username).first()
+        if not user:
+            raise HTTPException(status_code=401, detail="Token inválido")
+
+        return user
     except JWTError:
         raise HTTPException(status_code=401, detail="Token inválido o expirado")
