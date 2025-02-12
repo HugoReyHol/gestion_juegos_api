6661aae24160c7e47ad31a15162a23114aa3aa67
Hugo <hugo.reyhol@educa.jcyl.es>
2025-02-12 14:34:42 +0100

    cambiado input login

--- app/routers/user.py
+++ app/routers/user.py
@@ -42,11 +42,11 @@ async def read_users_me(current_user: User = Depends(get_current_user)):
         }
     }
 })
-def get_user(form_data: OAuth2PasswordRequestForm = Depends(), db:Session=Depends(get_db)):
-    user = db.query(User).filter(User.username == form_data.username).first()
+def get_user(user_req: UserRequest, db:Session=Depends(get_db)):
+    user = db.query(User).filter(User.username == user_req.username).first()
     if not user:
         raise HTTPException(status_code=404, detail="User not found")
-    if form_data.password != user.password:
+    if user_req.password != user.password:
         raise HTTPException(status_code=401, detail="Wrong credentials")
 
     access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
